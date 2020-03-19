92ac768863b3da1e4944691cf65dd867b177cdac
LiamZeng <37837011+LiamZeng@users.noreply.github.com>
2020-03-19 22:58:01 +0000

    1

---
+++ model2/SrSEBlock.py
@@ -0,0 +1,24 @@
+import torch.nn as nn
+from model2.SELayer import SELayer
+
+
+class SrSEBlock(nn.Module):
+    def __init__(self, planes, stride=1, use_se=True):
+        super(SrSEBlock, self).__init__()
+        self.use_se = use_se
+        self.conv1 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.relu = nn.PReLU()
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.se = SELayer(planes)
+
+    def forward(self, x):
+        residual = x
+        out = self.relu(self.conv1(x))
+        out = self.conv2(out)
+
+
+
+        if self.use_se:
+            out = self.se(out)
+        out += residual
+        return out
